89a698c820f5ec5b0c4d680b32d774cf8cf9d48b
agustin-197 <agustin041097@gmail.com>
2026-05-23 12:52:06 -0300

    Correccion: buffer de entrada

--- src/bsp.c
+++ src/bsp.c
@@ -114,19 +114,17 @@ static struct board_s board;
 /* === Private function implementations ============================================================ */
 
 static void BoardPinsConfigure(void) {
-    // Configuración de pines de los LEDs
-    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, LED_R_FUNC);
-    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, LED_G_FUNC);
-    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, LED_B_FUNC);
-    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, LED_1_FUNC);
-    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, LED_2_FUNC);
-    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, LED_3_FUNC);
+    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
+    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
+    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
+    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
+    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
+    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
 
-    // Configuración de pines de las Teclas
-    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, TEC_1_FUNC);
-    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, TEC_2_FUNC);
-    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, TEC_3_FUNC);
-    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, TEC_4_FUNC);
+    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
+    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
+    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
+    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
 }
 
 /* === Public function implementation ========================================================== */
